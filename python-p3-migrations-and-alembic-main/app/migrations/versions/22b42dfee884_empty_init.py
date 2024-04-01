bafbc95ed406dcfc7f7810e6fa54bcdbdd9f15b8
Leyian <victor.leyian@student.moringaschool.com>
2024-04-01 08:55:30 +0300

    done

---
+++ python-p3-migrations-and-alembic-main/app/migrations/versions/22b42dfee884_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 22b42dfee884
+Revises: 
+Create Date: 2024-03-24 06:20:48.989063
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '22b42dfee884'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
---
+++ python-p3-migrations-and-alembic-main/app/migrations/versions/cf183bae9fe7_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: cf183bae9fe7
+Revises: 22b42dfee884
+Create Date: 2024-03-24 06:22:50.523068
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'cf183bae9fe7'
+down_revision = '22b42dfee884'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
